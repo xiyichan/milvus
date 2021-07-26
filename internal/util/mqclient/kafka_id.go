c1fd533ff5443a33d204d6a88c30b1c2969f4d85
chenlifeng <chenlifeng0995@wezhuiyi.com>
2021-07-26 14:32:34 +0800

    kafka method finish

---
+++ internal/util/mqclient/kafka_id.go
@@ -0,0 +1,24 @@
+package mqclient
+
+import (
+	"encoding/binary"
+	"github.com/Shopify/sarama"
+)
+
+type kafkaID struct {
+	messageID *sarama.ConsumerMessage
+}
+
+func (kid *kafkaID) Serialize() []byte {
+	return SerializeKafkaID(kid.messageID.Offset)
+}
+
+func SerializeKafkaID(messageID int64) []byte {
+	b := make([]byte, 8)
+	binary.LittleEndian.PutUint64(b, uint64(messageID))
+	return b
+}
+
+func DeserializeKafkaID(messageID []byte) (int64, error) {
+	return int64(binary.LittleEndian.Uint64(messageID)), nil
+}
---
+++ internal/util/mqclient/kafka_producer.go
@@ -0,0 +1,35 @@
+package mqclient
+
+import (
+	"context"
+	"github.com/Shopify/sarama"
+	"github.com/milvus-io/milvus/internal/log"
+	"go.uber.org/zap"
+)
+
+type kafkaProducer struct {
+	p     sarama.SyncProducer
+	c     sarama.Client
+	topic string
+}
+
+func (kp *kafkaProducer) Topic() string {
+	return ""
+}
+func (kp *kafkaProducer) Send(ctx context.Context, message *ProducerMessage) error {
+	log.Info("send message topic ", zap.Any("topic", kp.topic))
+	msg := &sarama.ProducerMessage{Topic: kp.topic, Value: sarama.ByteEncoder(message.Payload)}
+	partition, offset, err := kp.p.SendMessage(msg)
+	if err != nil {
+		//log.Printf("FAILED to send message: %s\n", err)
+		log.Error("FAILED to send message", zap.Error(err))
+	} else {
+		log.Debug("> message sent to ", zap.Any("partition", partition), zap.Any("offset", offset))
+	}
+
+	return nil
+}
+
+func (kp *kafkaProducer) Close() {
+	kp.p.Close()
+}
